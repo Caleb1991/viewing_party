5303f956e58a8fbeeaede8b04b88467b067cd685
JoannaCoPo <jococopo@gmail.com>
2021-07-11 18:45:42 -0600

    Add assertion details to test

--- spec/features/viewing_party/new_spec.rb
+++ spec/features/viewing_party/new_spec.rb
@@ -20,23 +20,64 @@ RSpec.describe 'New viewing party page' do
     expect(page).to have_content('MOVIE TITLE PLACEHOLDER') #fix upon incorporating API
   end
 
-  it 'has field for duration of party with a default value of movie runtime in minutes' do
-    # expect(page).to
+  it 'has form to create new party redirecting user to their dashboard when filled in correctly' do
+    # need to add in this functionality: has field for duration of party with a default value of movie runtime in minutes
+    fill_in :duration, with: 122
+    fill_in :date, with: '2021-09-14'
+    fill_in :start_time, with: "8:00 PM"
+
+    check 'empanada_luvr@email.com'
+    check 'cheese_luvr@email.com'
+
+    click_button('Create Viewing Party')
+    expect(current_path).to eq(dashboard_path)
+
+    submitted_viewing_party = Party.last
+
+    expect(submitted_viewing_party.movie_title).to eq('PLACEHOLDER') #fix upon incorporating API
+    expect(submitted_viewing_party.duration).to eq(122)
+    expect(submitted_viewing_party.date).to eq('2021-09-14')
+    expect(submitted_viewing_party.start_time.to_time.strftime('%I:%M %p')).to eq("8:00 PM")
   end
 
-  it ' does not allow party creation if set to a value less than the duration of the movie' do
+  xit 'does not allow party creation if duration set to a value less than the duration of the movie' do
+    flash_message = 'Length of party can not be less than the movei length. Please put in a value that is at least the length of the movie.'
+
+    fill_in :duration, with: 60
+    fill_in :date, with: '2021-09-14'
+    fill_in :start_time, with: "8:00 PM"
+
+    check 'empanada_luvr@email.com'
+    check 'cheese_luvr@email.com'
+
+    click_button('Create Viewing Party')
+
+    expect(current_path).to eq(new_viewing_party_path)
+    expect(page).to have_content(flash_message)
   end
 
-  it 'has field for date and time of party' do
-  end
+  xit 'displays new event on user and invitees dashboards' do
+    fill_in :duration, with: 122
+    fill_in :date, with: '2021-09-14'
+    fill_in :start_time, with: "8:00 PM"
 
-  it 'has option to select firends to invite to party if use has friends' do
-  end
+    check 'cat_luvr@email.com'
 
-  it 'has a button to create new party and user is redirected back to dashboard upon party creation' do
-  end
+    click_button('Create Viewing Party')
+    expect(current_path).to eq(dashboard_path)
 
-  it 'displays new event on user and invitees dashboards' do
+    visit root_path
+
+    click_button 'Log out'
+    click_button 'Log me in!'
+
+    fill_in :email, with: 'cat_luvr@email.com'
+    fill_in :password, with: 'catz'
+
+    click_button 'Log In'
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content('MOVIE TITLE PLACEHOLDER')
   end
 end
 
