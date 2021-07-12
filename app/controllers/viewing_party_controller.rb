2e35f0733e43f9f19363d651eefca4fa84de0068
JoannaCoPo <jococopo@gmail.com>
2021-07-11 18:46:28 -0600

    Fix typo

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -1,7 +1,7 @@
 class ViewingPartyController < ApplicationController
   def new
     @party = Party.new
-    @friends = current_user.user_friendhips
+    @friends = current_user.user_friendships
   end
 
 end
